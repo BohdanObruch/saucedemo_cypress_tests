f866af00bf95b0d3f6a378e7408e4edce0fe15c5
Bohdan Obruch <bodan1992@gmail.com>
2024-07-11 13:05:15 +0300

    fixed data user and dependencies

--- cypress/support/shared.js
+++ cypress/support/shared.js
@@ -1,10 +1,10 @@
-import standard_user from "../fixtures/credentials.json";
+import user from "../fixtures/credentials.json";
 
 export function login() {
 
     cy.visit('/');
-    cy.get('#user-name').type(standard_user.username);
-    cy.get('#password').type(standard_user.password);
+    cy.get('#user-name').type(user.standardUser.username);
+    cy.get('#password').type(user.standardUser.password);
     cy.get('#login-button').click();
 
 }
